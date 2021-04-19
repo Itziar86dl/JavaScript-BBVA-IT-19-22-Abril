1c4b2b180e86777733b076fccd1599c7f6d737b0
alce65 <alejandro.cerezo@evosoft.es>
2021-04-19 11:35:28 +0200

    Hola Mundo

---
+++ 01_Hola_Mundo.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Comentario en linea
+
+/* Comentario
+Multilinea */
+ 
+
+function saludarHola() {
+    let miSaludo = 'Hola Mundo'
+    if (miSaludo) {
+        console.log(miSaludo)
+        console.log('Curso ES')
+    }
+}
+
+saludarHola()
